4703761ff6f40d3408ff548eaf3f366fae869252
mostafa <mostafa_mohamed12326@yahoo.com>
2024-05-07 06:33:54 +0300

    .

--- src/components/xr-cube/XrHitBox.jsx
+++ src/components/xr-cube/XrHitBox.jsx
@@ -7,6 +7,13 @@ import { useThree } from "@react-three/fiber";
 const XrHitBox = () => {
   const reticleRef = useRef();
   const [cubes, setCubes] = useState([]);
+  const { isPresenting } = useXR();
+
+  useThree(({ camera }) => {
+    if (!isPresenting) {
+      camera.position.z = 3;
+    }
+  });
 
   useHitTest((hitMatrix) => {
     // use hitMatrix to position any object on the real life surface
@@ -21,22 +28,17 @@ const XrHitBox = () => {
 
   const placeCube = (e) => {
     let position = e.intersection.object.position.clone();
+
     let id = Date.now();
     setCubes([...cubes, { position, id }]);
   };
-  const { isPresenting } = useXR();
-
-  useThree(({ camera }) => {
-    if (!isPresenting) {
-      camera.position.z = 3;
-    }
-  });
 
   return (
     <>
       <ambientLight />
       <OrbitControls />
-      {isPresenting && cubes.map((cube) => <XrCube key={cube.id} />)}
+      {isPresenting &&
+        cubes.map((cube) => <XrCube key={cube.id} position={cube.position} />)}
       {isPresenting && (
         <Interactive onSelect={placeCube}>
           <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
